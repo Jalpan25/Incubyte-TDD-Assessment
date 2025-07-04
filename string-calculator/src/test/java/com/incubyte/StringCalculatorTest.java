1ef3a51716d005adb4f029b1c80b10f0dbbae294
Jalpan25 <jalpanvvm@gmail.com>
2025-07-04 21:40:25 +0530

    Step 2 (Red): Add failing test for single number input '1' returning 1

--- string-calculator/src/test/java/com/incubyte/StringCalculatorTest.java
+++ string-calculator/src/test/java/com/incubyte/StringCalculatorTest.java
@@ -10,4 +10,9 @@ public class StringCalculatorTest {
         StringCalculator calc = new StringCalculator();
         assertEquals(0, calc.add(""));
     }
+    @Test
+    void testSingleNumberReturnsThatNumber() {
+        StringCalculator calc = new StringCalculator();
+        assertEquals(1, calc.add("1"));
+    }
 }
