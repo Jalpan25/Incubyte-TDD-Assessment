baf8c5a5fa62d93f5f63ab9114c67039cb84ba77
Jalpan25 <jalpanvvm@gmail.com>
2025-07-04 21:42:15 +0530

    Step 2 (Green): Parse and return single number input

--- string-calculator/src/main/java/com/incubyte/StringCalculator.java
+++ string-calculator/src/main/java/com/incubyte/StringCalculator.java
@@ -2,6 +2,9 @@ package com.incubyte;
 
 public class StringCalculator {
     public int add(String numbers) {
-        return 0; // minimum logic to pass the test
+        if (numbers.isEmpty()) {
+            return 0;
+        }
+        return Integer.parseInt(numbers);
     }
 }
